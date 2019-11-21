a016733514238d4b032d5376f99bdd0a74b00a71
iROCKBUNNY <irockbunny@outlook.com>
2019-11-22 06:41:56 +0800

    Update

--- Formula/nginx-vod-module.rb
+++ Formula/nginx-vod-module.rb
@@ -1,8 +1,8 @@
 class NginxVodModule < Formula
   desc "NGINX-based MP4 Repackager"
   homepage "https://github.com/kaltura/nginx-vod-module"
-  url "https://github.com/kaltura/nginx-vod-module/archive/1.24.tar.gz"
-  sha256 "b3cad8e3047556955fa028bcc68e7af46d19343942cb7984ad25e6f44d614ddf"
+  url "https://github.com/kaltura/nginx-vod-module/archive/1.25.tar.gz"
+  sha256 "42fd3b7f5bd4e13a7e27fcbd830153e285544c8cce37bfaea87cfce549f636d7"
 
   bottle :unneeded
 
